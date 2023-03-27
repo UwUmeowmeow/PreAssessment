8e08d39f089c37e9ec4ebc81c2985aba87eaf6d4
UwUmeowmeow <98915612+UwUmeowmeow@users.noreply.github.com>
2023-03-28 12:00:24 +1300

    Initial commit

---
+++ Menu1.py
@@ -0,0 +1,6 @@
+combos = [["value", ["beef burger", 5.69],["Fries", 1.00],["Fizzy drink", 1.00]],
+          ["Cheezy", ["Cheeseburger", 6.69],["Fries", 1.00],["Fizzy drink", 1.00]],
+          ["Super", ["Cheeseburger", 6.69],["Large Fries", 2.00],["Smoothie", 2.00]]]
+
+
+print(combos[2][2][1])
